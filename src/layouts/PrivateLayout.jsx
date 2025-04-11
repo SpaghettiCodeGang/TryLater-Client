83170daa600c92f5031867737d16d3dcd5b494a6
covox <lehmann.s7@gmx.de>
2025-04-11 16:04:02 +0200

    feat: adds base layout

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -3,10 +3,12 @@ import Navbar from '../components/Navbar';
 
 const PrivateLayout = ({ setIsLoggedIn }) => (
     <>
-        <main className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-            <Outlet />
-            <Navbar />
-            <button className="btn btn-primary mt-5" onClick={() => setIsLoggedIn(false)}>Logout (Dev)</button>
+        <main className="base-layout">
+            <div className="content-container">
+                <Outlet />
+                <Navbar />
+                <button className="btn btn-primary mt-5" onClick={() => setIsLoggedIn(false)}>Logout (Dev)</button>
+            </div>
         </main>
     </>
 );
--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
 const PublicLayout = ({ setIsLoggedIn }) => (
-    <main className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-        <Outlet />
-        <button className="btn btn-primary mt-4" onClick={() => setIsLoggedIn(true)}>Login (Dev)</button>
+    <main className="base-layout">
+        <div className="content-container">
+            <Outlet />
+            <button className="btn btn-primary mt-4" onClick={() => setIsLoggedIn(true)}>Login (Dev)</button>
+        </div>
     </main>
 );
 
