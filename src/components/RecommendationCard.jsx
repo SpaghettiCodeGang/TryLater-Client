8dacef29062323884a8afb293724ab675b94d2f8
covox <lehmann.s7@gmx.de>
2025-04-24 20:37:40 +0200

    feat: adds labels and fixes small bugs

--- src/components/RecommendationCard.jsx
+++ src/components/RecommendationCard.jsx
@@ -15,51 +15,57 @@ const RecommendationCard = ({ activeRecommendation, action }) => {
     return (
         <>
             <div className="recommendation-card">
-                <div className="recommendation-card_header" style={{backgroundImage: "url(" + imgSrc + ")"}}>
-                    <div className="recommendation-card_header__top">
-                        <span className="recommendation-card_tag">{activeRecommendation?.category?.slice(0, 1) + activeRecommendation?.category?.slice(1).toLowerCase()}</span>
-                        <div className="recommendation-card_rating">
-                            {Array.from({ length: activeRecommendation?.rating }).map((_, i) => (
-                                <BootstrapIcons.HeartFill key={i} width={18} height={18} />
-                            ))}
+                <div className="recommendation-card_inner">
+                    <div className="recommendation-card_header" style={{backgroundImage: "url(" + imgSrc + ")"}}>
+                        <div className="recommendation-card_header__top">
+                            <span className="recommendation-card_tag">
+                              {typeof activeRecommendation?.category === 'string'
+                                  ? activeRecommendation.category.charAt(0).toUpperCase() + activeRecommendation.category.slice(1).toLowerCase()
+                                  : ''}
+                            </span>
+                            <div className="recommendation-card_rating">
+                                {Array.from({ length: activeRecommendation?.rating }).map((_, i) => (
+                                    <BootstrapIcons.HeartFill key={i} width={18} height={18} />
+                                ))}
+                            </div>
+                        </div>
+                        <div className="recommendation-card_header__bottom">
+                            <span className="recommendation-card_tag">{activeRecommendation?.creator?.displayName} empfiehlt</span>
+                            <h2>{activeRecommendation?.title}</h2>
                         </div>
                     </div>
-                    <div className="recommendation-card_header__bottom">
-                        <span className="recommendation-card_tag">{activeRecommendation?.creator?.displayName} empfiehlt</span>
-                        <h2>{activeRecommendation?.title}</h2>
-                    </div>
-                </div>
 
-                <div className="recommendation-card_body">
-                    <div className="mb-3">
-                        <h3>Tags</h3>
-                        <div className="recommendation-card_tags">
-                            {activeRecommendation?.tagGroups?.map((tagGroup, t) =>
-                                tagGroup.tags.map((tag, i) => (
-                                    <div className="recommendation-card_tag recommendation-card_tag--gray" key={`${t}-${i}`}>
-                                        {tag.tagName}
-                                    </div>
-                                ))
-                            )}
+                    <div className="recommendation-card_body">
+                        <div className="mb-3">
+                            <label htmlFor="tags" className="form-label">Tags</label>
+                            <div className="recommendation-card_tags" id="tags">
+                                {activeRecommendation?.tagGroups?.map((tagGroup, t) =>
+                                    tagGroup.tags.map((tag, i) => (
+                                        <div className="recommendation-card_tag recommendation-card_tag--gray" key={`${t}-${i}`}>
+                                            {tag.tagName}
+                                        </div>
+                                    ))
+                                )}
+                            </div>
+
                         </div>
 
+                        <div className="mb-3">
+                            <label htmlFor="link" className="form-label">Link</label>
+                            <input type="text" className="form-control" id="link" disabled
+                                   placeholder={activeRecommendation?.url ? activeRecommendation?.url : "Kein Link vorhanden"} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="description" className="form-label">Notizen</label>
+                            <textarea className="form-control" id="description" disabled
+                                      placeholder={activeRecommendation?.description ? activeRecommendation?.description : "Keine Beschreibung vorhanden"} />
+                        </div>
                     </div>
 
-                    <div className="mb-3">
-                        <h3>Link</h3>
-                        <input className="form-control" type="text" disabled
-                               placeholder={activeRecommendation?.url ? activeRecommendation?.url : "Kein Link vorhanden"} />
+                    <div className="recommendation-card_footer">
+                        { action }
                     </div>
-
-                    <div className="mb-3">
-                        <h3>Notizen</h3>
-                        <textarea className="form-control" disabled
-                                  placeholder={activeRecommendation?.description ? activeRecommendation?.description : "Keine Beschreibung vorhanden"} />
-                    </div>
-                </div>
-
-                <div className="recommendation-card_footer">
-                    { action }
                 </div>
             </div>
         </>
