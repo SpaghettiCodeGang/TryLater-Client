501bf006dcfaaaf08fc8a723380e4835c3af18d7
Paul <99247256+PaulRiisk@users.noreply.github.com>
2025-04-30 00:36:51 +0200

    feat: added Homepage Styling part1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,33 @@ const HomePage = () => {
 
     return (
         <>
-            <p className="text-center"><Link to="/login">Login</Link> oder <Link to="/register">Registrieren</Link></p>
+            <h2 className="mb-2 text-center">Empfehlen. Swipen. <br /> Entdecken</h2>
+
+            <div className="d-flex justify-content-center my-4">
+                <div  style={{ width: '150px' }}>
+                    <img
+                        src="/assets/Homepage-mainimage.png"
+                        alt="Main"
+                        className="img-fluid p z-2"
+                    />
+                    <img
+                        src="/assets/Homepage-shadow.png"
+                        alt="Shadow"
+                        className=" mt-3 start-50 d-flex justify-content-center mg-fluid p z-2 z-1"
+                        style={{ width: '100px'}}
+                    />
+                </div>
+            </div>
+
+            <Link to="/register">
+            <button className="btn btn-primary form-control" type="submit">
+                    <span>Jetzt Registrieren!</span>
+            </button>
+            </Link>
+
+            <p className="text-center mb-5">
+                <small>Schon ein Account? <Link className="text-black" to="/login"><strong>Hier Einloggen</strong></Link></small>
+            </p>
         </>
     );
 };
