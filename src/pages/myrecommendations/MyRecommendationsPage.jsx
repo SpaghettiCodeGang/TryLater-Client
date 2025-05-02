7c8abf18e9a73ba6402358a3416b71cee96f93d4
covox <lehmann.s7@gmx.de>
2025-05-02 16:20:26 +0200

    feat: adds animation to my recommendation page

--- src/pages/myrecommendations/MyRecommendationsPage.jsx
+++ src/pages/myrecommendations/MyRecommendationsPage.jsx
@@ -4,6 +4,7 @@ import {useFetch} from "../../hooks/useFetch.jsx";
 import RecommendationCard from "../../components/RecommendationCard.jsx";
 import RecommendationPreviewComponent from "./RecommendationPreviewComponent.jsx";
 import apiService from "../../service/apiService.jsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 const MyRecommendationsPage = () => {
     const {setHeadline} = useLayout();
@@ -60,133 +61,150 @@ const MyRecommendationsPage = () => {
         } finally {
             setTimeout(() => {
                 setLoading(false);
-                /*setActiveCard(false);*/
+                setActiveCard(false);
             }, loadingAnimationDuration);
         }
     }
 
     return (
         <>
+            <AnimatePresence mode="wait">
+                {!activeCard && (
+                    <motion.div
+                        className="main-content"
+                        key="main-content"
+                        exit={{ y: "-100%" }}
+                        transition={{ duration: 0.15 }}
+                    >
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <h3>Media</h3>
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" className="collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <ul>
+                                            {medias?.map((media) => (
+                                                <RecommendationPreviewComponent
+                                                    key={media.id}
+                                                    recommendation={media}
+                                                    onClick={() => handleActive(media)}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <h3>Locations</h3>
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" className="collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <ul>
+                                            {locations?.map((location) => (
+                                                <RecommendationPreviewComponent
+                                                    key={location.id}
+                                                    recommendation={location}
+                                                    onClick={() => handleActive(location)}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseThree" aria-expanded="false"
+                                            aria-controls="collapseThree">
+                                        <h3>Rezepte</h3>
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" className="collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <ul>
+                                            {recipes?.map((recipe) => (
+                                                <RecommendationPreviewComponent
+                                                    key={recipe.id}
+                                                    recommendation={recipe}
+                                                    onClick={() => handleActive(recipe)}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseFour" aria-expanded="false"
+                                            aria-controls="collapseFour">
+                                        <h3>Produkte</h3>
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" className="collapse"
+                                     data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
+                                        <ul>
+                                            {products?.map((product) => (
+                                                <RecommendationPreviewComponent
+                                                    key={product.id}
+                                                    recommendation={product}
+                                                    onClick={() => handleActive(product)}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
-            {!activeCard ? (
-                <>
-                    <div className="accordion" id="accordionExample">
-                        <div className="accordion-item">
-                            <h2 className="accordion-header">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    <h3>Media</h3>
+            <AnimatePresence mode="wait">
+                {activeCard && (
+                    <motion.div
+                        className="main-content"
+                        key="recommendation-card"
+                        initial={{ y: "100%", scale: 0 }}
+                        animate={{ y: "0%", scale: 1 }}
+                        exit={{ y: "100%", scale: 0 }}
+                        transition={{ duration: 0.4 }}
+                    >
+                        <RecommendationCard
+                            activeRecommendation={activeCard}
+                            error={error}
+                            onClose={<button onClick={handleClose}>X</button>}
+                            action={
+                                <button className="btn btn-primary form-control" type="submit" disabled={loading}
+                                        onClick={() => {
+                                            void handleDelete(activeCard.id)
+                                        }}>
+                                    {loading ? (
+                                        <>
+                                            <span className="btn-spinner me-2"/>
+                                            <span>Sende...</span>
+                                        </>
+                                    ) : (
+                                        <span>Erledigt</span>
+                                    )}
                                 </button>
-                            </h2>
-                            <div id="collapseOne" className="collapse"
-                                 data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <ul>
-                                        {medias?.map((media) => (
-                                            <RecommendationPreviewComponent
-                                                key={media.id}
-                                                recommendation={media}
-                                                onClick={() => handleActive(media)}
-                                            />
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    <h3>Locations</h3>
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" className="collapse"
-                                 data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <ul>
-                                        {locations?.map((location) => (
-                                            <RecommendationPreviewComponent
-                                                key={location.id}
-                                                recommendation={location}
-                                                onClick={() => handleActive(location)}
-                                            />
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseThree" aria-expanded="false"
-                                        aria-controls="collapseThree">
-                                    <h3>Rezepte</h3>
-                                </button>
-                            </h2>
-                            <div id="collapseThree" className="collapse"
-                                 data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <ul>
-                                        {recipes?.map((recipe) => (
-                                            <RecommendationPreviewComponent
-                                                key={recipe.id}
-                                                recommendation={recipe}
-                                                onClick={() => handleActive(recipe)}
-                                            />
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseFour" aria-expanded="false"
-                                        aria-controls="collapseFour">
-                                    <h3>Produkte</h3>
-                                </button>
-                            </h2>
-                            <div id="collapseFour" className="collapse"
-                                 data-bs-parent="#accordionExample">
-                                <div className="accordion-body">
-                                    <ul>
-                                        {products?.map((product) => (
-                                            <RecommendationPreviewComponent
-                                                key={product.id}
-                                                recommendation={product}
-                                                onClick={() => handleActive(product)}
-                                            />
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <RecommendationCard
-                    activeRecommendation={activeCard}
-                    error={error}
-                    onClose={<button onClick={handleClose}>X</button>}
-                    action={
-                        <button className="btn btn-primary form-control" type="submit" disabled={loading}
-                                onClick={() => {
-                                    void handleDelete(activeCard.id)
-                                }}>
-                            {loading ? (
-                                <>
-                                    <span className="btn-spinner me-2"/>
-                                    <span>Sende...</span>
-                                </>
-                            ) : (
-                                <span>Erledigt</span>
-                            )}
-                        </button>
-                    }
-                >
-
-                </RecommendationCard>
-            )}
+                            }
+                        />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     )
 }
